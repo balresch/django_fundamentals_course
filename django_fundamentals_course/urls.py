acf090aaf9e4eb7fc5d4ec4f1f79f682ffa21e3e
balresch <balresch@gmail.com>
2018-11-21 15:14:32 +0100

    chapter 5.5, first successful migration

--- django_fundamentals_course/urls.py
+++ django_fundamentals_course/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import welcome
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome', welcome),
+    url(r'^$', welcome)
 ]
